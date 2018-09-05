204995054c0663534e1ef1748fa8992e73e6b77a
Mats Rauhala <mats.rauhala@iki.fi>
2018-09-05 13:27:20 +0300

    Hoogle
    Change-Id: Ib66ac8efdfce249e47f0d32ade95b8e6afe48da3

--- zsh/gwp/gwp.plugin.zsh
+++ zsh/gwp/gwp.plugin.zsh
@@ -1,4 +1,9 @@
 function gwp() {
    pkgs="$@"
-   nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [$pkgs])" --run 'ghci'
+   nix-shell -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'zsh'
+}
+
+function gwi() {
+   pkgs="$@"
+   nix-shell -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'ghci'
 }
