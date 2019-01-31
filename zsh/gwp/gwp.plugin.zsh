cc9283a76e1a83dedf9b1c20126d81439a4a03c8
Mats Rauhala <mats.rauhala@iki.fi>
2019-01-31 16:07:40 +0200

    Fix shell
    Change-Id: I70c184790521cae9879e1c0453c1bd7f77fbe6e8

--- zsh/gwp/gwp.plugin.zsh
+++ zsh/gwp/gwp.plugin.zsh
@@ -1,9 +1,9 @@
 function gwp() {
    pkgs="$@"
-   nix-shell -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'zsh'
+   nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'zsh'
 }
 
 function gwi() {
    pkgs="$@"
-   nix-shell -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'ghci'
+   nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'ghci'
 }
