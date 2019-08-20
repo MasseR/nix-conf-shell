0944405a273bf832dfe1a966914160e9ee5e9e97
Mats Rauhala <mats.rauhala@iki.fi>
2019-08-20 14:53:32 +0300

    Hard-code ghcid
    Change-Id: Idae76b75dc8200b47b093b6c06779b0be2f30af8

--- zsh/gwp/gwp.plugin.zsh
+++ zsh/gwp/gwp.plugin.zsh
@@ -1,6 +1,6 @@
 function gwp() {
    pkgs="$@"
-   nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'zsh'
+   nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz -p "ghcid" -p "haskellPackages.ghcWithHoogle (pkgs: with pkgs; [$pkgs])" --run 'zsh'
 }
 
 function gwi() {
